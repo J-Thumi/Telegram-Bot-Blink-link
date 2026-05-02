0d3634be3ce1c390358bb9b644cde516a6b01edd
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2026-05-02 12:55:50 +0300

    Fix:File upload error

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-request', function () {
+    return [
+        'url' => request()->fullUrl(),
+        'host' => request()->getHost(),
+        'scheme' => request()->getScheme(),
+        'isSecure' => request()->isSecure(),
+        'headers' => request()->headers->all(),
+    ];
+});
